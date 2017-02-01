ec7b2e1398c7303068f9f36914dba9a48f8f42d8
BayMinimum <bearksb1115@gmail.com>
2017-02-02 01:36:23 +0900

    Fixed determining meal type

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ function repeat() {
     var now=new time.Date();
     now.setTimezone("Asia/Seoul");
     const yyyy=now.getFullYear();
-    const mm=now.getMonth();
-    const dd=now.getDay();
+    const mm=now.getMonth()+1;
+    const dd=now.getDate();
     const h=now.getHours();
     const m=now.getMinutes();
     var m_total=60*h+m;
 
     i=-1;
     if(m_total<60) i=0; // morning
-    else if (11*60+40<=m_total<12*60+40) i=1; // lunch
-    else if (17*60+30<=m_total<18*60+30) i=2; // dinner
+    else if (11*60+40<=m_total && m_total<12*60+40) i=1; // lunch
+    else if (17*60+30<=m_total && m_total<18*60+30) i=2; // dinner
 
     if(i==-1) return false;
     else try {
