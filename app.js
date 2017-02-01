3fe8e0e46c6d5c65d11645c280fecc9115e26797
BayMinimum <bearksb1115@gmail.com>
2017-02-02 00:44:36 +0900

    app.js tweet meal data from meal.js; added test mode

---
+++ app.js
@@ -0,0 +1,63 @@
+// tweet meal data from meal.js
+// when meal time
+var meal=require('./meal');
+var time=require("time");
+const __test__=true;
+
+function repeat() {
+    // check time
+    var now=new time.Date();
+    now.setTimezone("Asia/Seoul");
+    const yyyy=now.getFullYear();
+    const mm=now.getMonth();
+    const dd=now.getDay();
+    const h=now.getHours();
+    const m=now.getMinutes();
+    var m_total=60*h+m;
+
+    i=-1;
+    if(m_total<60) i=0; // morning
+    else if (11*60+40<=m_total<12*60+40) i=1; // lunch
+    else if (17*60+30<=m_total<18*60+30) i=2; // dinner
+
+    if(i==-1) return false;
+    else meal(function (meals) {
+        const mealType=["조식", "중식", "석식"];
+        const text=`${yyyy}/${mm}/${dd} ${mealType[i]}\n${meals[i]}`;
+        console.log("Trying to tweet...");
+        console.log(text);
+        var Twitter=require('twitter');
+        var twitter_key={
+            consumer_key: process.env.TWITTER_CONSUMER_KEY,
+            consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+            access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+            access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+        };
+        if(__test__) twitter_key=require('./twitter_key');
+        var client=new Twitter(twitter_key);
+        client.post('statuses/update', {status:text}, function (err, tweet, res) {
+            if(err){
+                console.log("Tweet failed!");
+                console.log(err);
+                return false;
+            }
+            else{
+                console.log("Tweet success!");
+                clearInterval(interval);
+                return true;
+            }
+        });
+    })
+}
+
+if(!repeat()) var interval=setInterval(repeat, 10*60*1000);
+
+// http server to keep bot alive
+var http=require('http');
+http.createServer((req, res)=>{
+    res.writeHead(200);
+    res.write('<html><head><title>MealTweet Bot</title></head><body>This bot is running:)');
+    res.end('</body></html>');
+}).listen(8080);
+
+console.log("Listening on port 8080");
--- meal.js
+++ meal.js
@@ -1,52 +1,62 @@
 module.exports= function (callback) {
     var cheerio = require('cheerio');
     var https = require('https');
+    const __test__=true;
 
-    var options = {
-        host: "ksa.hs.kr",
-        path: "/Home/CafeteriaMenu/72"
-    };
+    if (!__test__) {
+        var options = {
+            host: "ksa.hs.kr",
+            path: "/Home/CafeteriaMenu/72"
+        };
 
-    var data = "";
-    var request = https.request(options, function (res) {
-        res.setEncoding("utf8");
-        res.on('data', function (chunk) {
-            data += chunk;
-            console.log("received chunk");
+        // get html data from school website
+        var data = "";
+        var request = https.request(options, function (res) {
+            res.setEncoding("utf8");
+            res.on('data', function (chunk) {
+                data += chunk;
+                console.log("received chunk");
+            });
+            res.on('end', function () {
+                parseMeal(data);
+            });
         });
-        res.on('end', function () {
-            parseMeal(data);
+
+        request.on('error', function () {
+            throw new Error("Network error");
         });
-    });
 
-    request.on('error', function () {
-        throw new Error("Network error");
-    });
+        request.end();
+    }
 
-    request.end();
-
-
+    // pass meal as [breakfast, lunch, dinner] to callback func
     var parseMeal = function (html) {
         meals = [];
-        $ = cheerio.load(html, {decodeEntities: false});
+        $ = cheerio.load(html, {decodeEntities: false}); // option to avoid unicode hangul issue
         $(".meal").find('ul').each((i, elem) => {
             var meal = "";
             $(elem).find('li').each((j, elem) => {
-                    meal += $(elem).toString('utf8')
+                    meal += $(elem).toString()
                         .replace("<li>", "")
                         .replace("</li>", "")
-                        .replace(/ /g, "");
+                        .replace(/ /g, "")
+                        .replace(/amp\;/g, "");
                 }
             );
+            // remove \n in start or end
+            if(meal.charAt(0)=='\n') meal.replace('\n', "");
+            if(meal.charAt(meal.length)=='\n') meal=meal.substring(0, meal.length-1);
             meals.push(meal);
         });
         callback(meals);
     };
 
     // test using local copy
-    var fs = require('fs');
-    fs.readFile('/home/baymax/WebstormProjects/MealTweet/test.html', (err, data) => {
-        if (err) throw err;
-        parseMeal(data.toString());
-    });
+    if(__test__) {
+        var fs = require('fs');
+        fs.readFile('/home/baymax/WebstormProjects/MealTweet/test.html', (err, data) => {
+            if (err) throw err;
+            parseMeal(data.toString());
+        });
+    }
 };
